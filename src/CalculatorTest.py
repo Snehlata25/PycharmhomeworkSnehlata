ba17ba8b0b8f651a0096eb5e0977bd98548a55c7
Snehlata25 <sneh25@gmail.com>
2019-10-19 21:39:32 -0400

    Feature: added multiplication and division

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -13,10 +13,18 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.add(1, 1), 2)
         self.assertEqual(self.calculator.result, 2)
 
-
     def test_subtraction(self):
         self.assertEqual(self.calculator.subtract(1, 1), 0)
         self.assertEqual(self.calculator.result, 0)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calculator.multiply(2, 3), 6)
+        self.assertEqual(self.calculator.result, 6)
+
+    def test_division(self):
+        self.assertEqual(self.calculator.divide(6, 2), 3)
+        self.assertEqual(self.calculator.result, 3)
+
 
 
     def test_results_property(self):
