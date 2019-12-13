69143c804a292b99ae1a067efcd150e54849e9fc
Snehlata25 <sneh25@gmail.com>
2019-12-13 13:21:44 -0500

    Feature: Inserted new customers

--- Database/sqlite_create.py
+++ Database/sqlite_create.py
@@ -5,21 +5,21 @@ conn = sqlite3.connect('/web/Sqlite-Data/example.db')
 
 c = conn.cursor()
 c.execute('''
-          CREATE TABLE person
+          CREATE TABLE Customer
           (id INTEGER PRIMARY KEY ASC, name varchar(250) NOT NULL)
           ''')
 c.execute('''
-          CREATE TABLE address
-          (id INTEGER PRIMARY KEY ASC, street_name varchar(250), street_number varchar(250),
-           post_code varchar(250) NOT NULL, person_id INTEGER NOT NULL,
-           FOREIGN KEY(person_id) REFERENCES person(id))
+          CREATE TABLE Item
+          (id INTEGER PRIMARY KEY ASC, name varchar(250), cost_price FLOAT NOT NULL,
+           selling_price FLOAT NOT NULL, quantity INTEGER NOT NULL,
+           FOREIGN KEY(customer_id) REFERENCES customer(id))
           ''')
 
 c.execute('''
-          INSERT INTO person VALUES(1, 'pythoncentral')
+          INSERT INTO customer VALUES(1, 'pythoncentral')
           ''')
 c.execute('''
-          INSERT INTO address VALUES(1, 'python road', '1', '00000', 1)
+          INSERT INTO item VALUES(1, 'python road', '1', '00000', 1)
           ''')
 
 conn.commit()
